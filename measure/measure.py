f26e258cc2e34c88151db05790f46993af819e21
James Whang <sungyoonwhang2017@u.northwestern.edu>
2017-02-06 16:43:29 -0600

    Adding measurement script and WP AMP sites

---
+++ measure/measure.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""
+Use Selenium to Measure Web Timing
+
+  Performance Timing Events flow
+
+   navigationStart -> redirectStart -> redirectEnd -> fetchStart -> domainLookupStart -> domainLookupEnd 
+     -> connectStart -> connectEnd -> requestStart -> responseStart -> responseEnd 
+       -> domLoading -> domInteractive -> domContentLoaded -> domComplete -> loadEventStart -> loadEventEnd
+"""
+
+from selenium import webdriver
+
+
+SRC = 'wp_sites.txt'
+
+def measure(driver, site):
+    driver.get(site)
+    navigationStart = driver.execute_script("return window.performance.timing.navigationStart")    
+    eventEnd = driver.execute_script("return window.performance.timing.loadEventEnd")
+
+    requestStart = driver.execute_script("return window.performance.timing.requestStart")
+    responseEnd = driver.execute_script("return window.performance.timing.responseEnd")
+
+    totalTime = eventEnd - navigationStart
+    responseTime = responseEnd - requestStart
+
+    return str(totalTime), str(responseTime)
+
+
+if __name__ == '__main__':
+
+    driver = webdriver.Chrome()
+
+    print 'link,non_amp_total,non_amp_response,amp_total,amp_response'
+
+    sites = open(SRC).readlines()
+    for site in sites:
+        site = site.replace('\n', '')
+
+        non_amp = site.split(',')[0]
+        amp = site.split(',')[1].replace('\n', '')
+
+        non_amp_totaltime, non_amp_responsetime = measure(driver, non_amp)
+        amp_totaltime, amp_responsetime = measure(driver, amp)
+
+        print site + ',' + non_amp_totaltime + ',' + non_amp_responsetime + ',' + amp_totaltime + ',' + amp_responsetime + ''
+        
+
+    driver.quit()
