a3b20be70334cf5ba9c81d1477c7a0368715b027
James Whang <sungyoonwhang2017@u.northwestern.edu>
2017-02-06 20:08:13 -0600

    more WP crawls

--- WP/crawl_wp.py
+++ WP/crawl_wp.py
@@ -1,32 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
+INPUT_FILE = open('wp_subdirectories.txt').readlines()
 OUTPUT_FILE = open('WPLinks.txt', 'wb')
 WP_URL = 'https://www.washingtonpost.com/'
 
-wp_home = requests.get(WP_URL)
-wp_home_s = BeautifulSoup(wp_home.text)
-
-all_links = wp_home_s.find_all('a')
-
 links = []
-
 written_links = []
 
 
-for link in all_links:
-    try:
-        href = link['href']
-        if href.startswith('https://www.washingtonpost.com/news/'):
-            links.append(href)
-    except Exception:
-        pass
+def search_url(url):
+    wp_home = requests.get(WP_URL)
+    wp_home_s = BeautifulSoup(wp_home.text)
 
-for link in links:
-    newslink = link.split('https://www.washingtonpost.com/')[1]
-    amp_link = 'https://www.washingtonpost.com/amphtml/' + newslink
+    all_links = wp_home_s.find_all('a')
 
-    if requests.get(amp_link).status_code == 200:
-        if link not in written_links:
-            print '{},{}'.format(link, amp_link)
-            written_links.append(link)
+
+
+
+    for link in all_links:
+        try:
+            href = link['href']
+            if href.startswith('https://www.washingtonpost.com/news/'):
+                links.append(href)
+        except Exception:
+            pass
+
+    for link in links:
+        newslink = link.split('https://www.washingtonpost.com/')[1]
+        amp_link = 'https://www.washingtonpost.com/amphtml/' + newslink
+
+        if requests.get(amp_link).status_code == 200:
+            if link not in written_links:
+                print link + ',' + amp_link
+                written_links.append(link)
+
+
+if __name__ == '__main__':
+    for url in INPUT_FILE: 
+        search_url(url.replace('\n', ''))
