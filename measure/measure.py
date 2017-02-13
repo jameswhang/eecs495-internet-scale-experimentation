9b913e0fa93721dc3db5652c1dcee50edd952905
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2017-02-13 16:27:29 -0600

    Better dns cache flushing at OS level

--- measure/measure.py
+++ measure/measure.py
@@ -12,10 +12,18 @@ Use Selenium to Measure Web Timing
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from time import sleep
+import os
 
-SRC = 'wp_links_v2.txt'
+SRC = '/Users/jameswhang/Documents/school/eecs495-ise/eecs495-internet-scale-experimentation/sites/measure_2017_02_13.txt'
+
+ATTEMPT_TIMES = 3
 
 def measure(site):
+    # First flush DNS Cache
+    os.system('sudo /Users/jameswhang/Documents/school/eecs495-ise/eecs495-internet-scale-experimentation/measure/dnsflush.sh')
+
+    # Fake User Agent as mobile Safari
     opts = Options()
     opts.add_argument("user-agent=Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>")
     driver = webdriver.Chrome(chrome_options=opts)
@@ -35,7 +43,7 @@ def measure(site):
 
 
 if __name__ == '__main__':
-    print 'link,non_amp_total,non_amp_response,amp_total,amp_response'
+    print 'link,non_amp_total,non_amp_response,amp_total,amp_response,attempt#'
 
     sites = open(SRC).readlines()
     for site in sites:
@@ -44,7 +52,9 @@ if __name__ == '__main__':
         non_amp = site.split(',')[0]
         amp = site.split(',')[1].replace('\n', '')
 
-        non_amp_totaltime, non_amp_responsetime = measure(non_amp)
-        amp_totaltime, amp_responsetime = measure(amp)
+        for i in range(ATTEMPT_TIMES):
+            amp_totaltime, amp_responsetime = measure(amp)
+            non_amp_totaltime, non_amp_responsetime = measure(non_amp)
+            print site + ',' + non_amp_totaltime + ',' + non_amp_responsetime + ',' + amp_totaltime + ',' + amp_responsetime + ',' + str(i)
+            sleep(5)  # sleep 5 sec and try again
 
-        print site + ',' + non_amp_totaltime + ',' + non_amp_responsetime + ',' + amp_totaltime + ',' + amp_responsetime + ''
