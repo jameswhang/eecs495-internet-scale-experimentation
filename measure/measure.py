e65013d62a6f93772a0eb22400999c1e6e4a0ed5
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2017-02-28 23:53:32 -0600

    Add firefox

--- measure/measure.py
+++ measure/measure.py
@@ -13,17 +13,19 @@ Use Selenium to Measure Web Timing
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+from datetime import date
 import os
 
 SRC = '/Users/jameswhang/Documents/school/eecs495-ise/eecs495-internet-scale-experimentation/sites/measure_2017_02_21.txt'
 
-ATTEMPT_TIMES = 3
+ATTEMPT_TIMES = 1
 
 def measure(site):
     # First flush DNS Cache
     os.system('sudo /Users/jameswhang/Documents/school/eecs495-ise/eecs495-internet-scale-experimentation/measure/dnsflush.sh')
 
     # Fake User Agent as mobile Safari
+    """
     opts = Options()
     opts.add_argument("user-agent=Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>")
     driver = webdriver.Chrome(chrome_options=opts)
@@ -34,16 +36,42 @@ def measure(site):
     requestStart = driver.execute_script("return window.performance.timing.requestStart")
     responseEnd = driver.execute_script("return window.performance.timing.responseEnd")
 
-    totalTime = eventEnd - navigationStart
-    responseTime = responseEnd - requestStart
+    chromeTotalTime = eventEnd - navigationStart
+    chromeResponseTime = responseStart - requestStart
+    driver.quit()
+    """
 
+    # Firefox Measurement
+    firefox_profile = webdriver.FirefoxProfile()
+    firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
+    firefox_profile.set_preference("general.useragent.override", "Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>")
+    driver = webdriver.Firefox(firefox_profile=firefox_profile)
+    driver.get(site)
+    navigationStart = driver.execute_script("return window.performance.timing.navigationStart")    
+    eventEnd = driver.execute_script("return window.performance.timing.loadEventEnd")
+
+    requestStart = driver.execute_script("return window.performance.timing.requestStart")
+    responseEnd = driver.execute_script("return window.performance.timing.responseEnd")
+
+    firefoxTotalTime = eventEnd - navigationStart
+    firefoxResponseTime = responseStart - requestStart
     driver.quit()
 
-    return str(totalTime), str(responseTime)
+    return {
+        'Chrome': [str(chromeTotalTime), str(chromeResponseTime)],
+        'Firefox': [str(firefoxTotalTime), str(firefoxResponseTime)],
+    }
 
 
 if __name__ == '__main__':
-    print 'link,non_amp_total,non_amp_response,amp_total,amp_response,attempt#'
+
+    todaystr = str(date.today()).replace('-', '_')
+
+    chrome_output = open('./measurement_chrome_{today}'.format(today=todaystr), 'wb')
+    firefox_output = open('./measurement_firefox_{today}'.format(today=todaystr), 'wb')
+
+    chrome_output.write('link,non_amp_total,non_amp_response,amp_total,amp_response,attempt#\n')
+    firefox_output.write('link,non_amp_total,non_amp_response,amp_total,amp_response,attempt#\n')
 
     sites = open(SRC).readlines()
     for site in sites:
@@ -54,17 +82,22 @@ if __name__ == '__main__':
 
         for i in range(ATTEMPT_TIMES):
             try:
-                amp_totaltime, amp_responsetime = measure(amp)
+                amp_res = measure(amp)
             except Exception:
-                amp_totaltime = 'ERROR'
-                amp_responsetime = 'ERROR'
-                sleep(5)  # sleep 5 sec and try again
+                amp_res = None
+                # Might be a timeout exception, so will sleep a little
+                sleep(5)
             try:
-                non_amp_totaltime, non_amp_responsetime = measure(non_amp)
+                namp_res = measure(non_amp)
             except Exception:
-                sleep(5)  # sleep 5 sec and try again
-                non_amp_totaltime = 'ERROR'
-                non_amp_responsetime = 'ERROR'
-            print site + ',' + non_amp_totaltime + ',' + non_amp_responsetime + ',' + amp_totaltime + ',' + amp_responsetime + ',' + str(i)
+                namp_res = None 
+
+            if amp_res is not None and namp_res is not None:
+                chrome_res = amp + ',' + non_amp + ',' + namp_res['Chrome'][0] + ',' + namp_res['Chrome'][1] + ',' + amp_res['Chrome'][0] + ',' + amp_res['Chrome'][1] + ',' + str(i) + '\n'
+                firefox_res = amp + ',' + non_amp + ',' + namp_res['Firefox'][0] + ',' + namp_res['Firefox'][1] + ',' + amp_res['Firefox'][0] + ',' + amp_res['Firefox'][1] + ',' + str(i) + '\n'
+
+                chrome_output.write(chrome_res)
+                firefox_output.write(firefox_res)
+
             sleep(5)  # sleep 5 sec and try again
 
